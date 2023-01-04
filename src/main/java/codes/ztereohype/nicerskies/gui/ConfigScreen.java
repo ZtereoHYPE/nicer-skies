8bf9f170312436137f6e5c36ce763ccf66e39efd
MicrocontrollersDev <66657148+MicrocontrollersDev@users.noreply.github.com>
2023-01-03 20:15:23 -0700

    Fix typo

--- src/main/java/codes/ztereohype/nicerskies/gui/ConfigScreen.java
+++ src/main/java/codes/ztereohype/nicerskies/gui/ConfigScreen.java
@@ -39,7 +39,7 @@ public class ConfigScreen extends Screen {
             }
         });
 
-        addRenderableWidget(new Checkbox(20, 84, 20, 20, Component.literal("Twinlke Stars"), cm.getTwinklingStars()) {
+        addRenderableWidget(new Checkbox(20, 84, 20, 20, Component.literal("Twinkle Stars"), cm.getTwinklingStars()) {
             @Override
             public void onPress() {
                 super.onPress();
