de85e17689a9e801901b95fe3f2e7e69ac4991e7
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2023-03-15 13:53:50 +0100

    change: finish update to 1.19.4

--- src/main/java/codes/ztereohype/nicerskies/gui/ModMenuSettingsApi.java
+++ src/main/java/codes/ztereohype/nicerskies/gui/ModMenuSettingsApi.java
@@ -1,14 +1,14 @@
 package codes.ztereohype.nicerskies.gui;
 
-//import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-//import com.terraformersmc.modmenu.api.ModMenuApi;
-//import net.fabricmc.api.EnvType;
-//import net.fabricmc.api.Environment;
-//
-//@Environment(EnvType.CLIENT)
-//public class ModMenuSettingsApi implements ModMenuApi {
-//    @Override
-//    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-//        return ConfigScreen::new;
-//    }
-//}
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ModMenuSettingsApi implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return ConfigScreen::new;
+    }
+}
