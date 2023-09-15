0a1aac900d743f4e6fba41338df1d374f6762691
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2023-09-16 01:23:44 +0200

    refactor config screen and system to be much more robust
    (cherry picked)

--- src/main/java/codes/ztereohype/nicerskies/config/Config.java
+++ src/main/java/codes/ztereohype/nicerskies/config/Config.java
@@ -1,39 +1,142 @@
 package codes.ztereohype.nicerskies.config;
 
+import codes.ztereohype.nicerskies.NicerSkies;
+import com.google.gson.Gson;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
-@Data
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Config {
-    private boolean tweakedLigthmap;
-    private boolean twinklingStars;
-    private boolean nebulas;
+    public static final ConfigData DEFAULT_CONFIG = new ConfigData(false,
+                                                                   true,
+                                                                   true,
+                                                                   NebulaType.RAINBOW.getTypeString(),
+                                                                   1f,
+                                                                   0.5f,
+                                                                   1f,
+                                                                   128,
+                                                                   false);
 
-    private final NebulaConfig nebulaConfig;
+    private static final Gson gson = new Gson();
+    private final File file;
 
-    public Config(boolean tweakedLigthmap, boolean twinklingStars, boolean nebulas, String nebulaType, float nebulaStrength, float nebulaNoiseAmount, float nebulaNoiseScale, int baseColourAmount, boolean renderDuringDay) {
-        this.tweakedLigthmap = tweakedLigthmap;
-        this.twinklingStars = twinklingStars;
-        this.nebulas = nebulas;
+    @Getter
+    private ConfigData configData;
 
-        this.nebulaConfig = new NebulaConfig(nebulaType, nebulaStrength, nebulaNoiseAmount, nebulaNoiseScale, baseColourAmount, renderDuringDay);
+    public static Config fromFile(File file) {
+        ConfigData config;
+        try {
+            if (!file.exists()) {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+
+                config = DEFAULT_CONFIG.toBuilder().build();
+
+                gson.toJson(config, new FileWriter(file));
+            } else {
+                config = gson.fromJson(new FileReader(file), ConfigData.class);
+
+                if (config == null) {
+                    throw new IOException("Failed to read config file");
+                }
+            }
+        } catch (IOException e) {
+            NicerSkies.LOGGER.warning("Failed to read config file, falling back to default config.");
+            e.printStackTrace();
+            config = DEFAULT_CONFIG.toBuilder().build();
+        }
+
+        return new Config(config, file);
+    }
+
+    private Config(ConfigData configData, File file) {
+        this.configData = configData;
+        this.file = file;
+        save(file);
+    }
+
+    public boolean getLightmapTweaked() {
+        return configData.isLightmapTweaked();
+    }
+
+    public boolean areTwinlkingStarsEnabled() {
+        return configData.isTwinklingStars();
+    }
+
+    public boolean areNebulasEnabled() {
+        return configData.isRenderNebulas();
+    }
+
+    public float getNebulaStrength() {
+        return configData.getNebulaConfig().getNebulaStrength();
+    }
+
+    public float getNebulaNoiseAmount() {
+        return configData.getNebulaConfig().getNebulaNoiseAmount();
+    }
+
+    public float getNebulaNoiseScale() {
+        return configData.getNebulaConfig().getNebulaNoiseScale();
+    }
+
+    public int getNebulaBaseColourAmount() {
+        return configData.getNebulaConfig().getBaseColourAmount();
+    }
+
+    public boolean getRenderDuringDay() {
+        return configData.getNebulaConfig().isRenderDuringDay();
+    }
+
+    public void updateConfig(ConfigData configData) {
+        this.configData = configData.toBuilder().build();
+        this.configData.setNebulaConfig(configData.getNebulaConfig().toBuilder().build());
+        save(file);
+    }
+
+    public void save(File file) {
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(gson.toJson(configData));
+        } catch (IOException e) {
+            e.printStackTrace();
+            NicerSkies.LOGGER.warning("Failed to save config file!");
+        }
     }
 
     @Data
-    public static final class NebulaConfig {
-        public NebulaConfig(String nebulaType, float nebulaStrength, float nebulaNoiseAmount, float nebulaNoiseScale, int baseColourAmount, boolean renderDuringDay) {
-            this.nebulaType = nebulaType;
-            this.nebulaStrength = nebulaStrength;
-            this.nebulaNoiseAmount = nebulaNoiseAmount;
-            this.nebulaNoiseScale = nebulaNoiseScale;
-            this.baseColourAmount = baseColourAmount;
-            this.renderDuringDay = renderDuringDay;
+    @AllArgsConstructor
+    @Builder(toBuilder=true)
+    public final static class ConfigData {
+        private boolean lightmapTweaked;
+        private boolean twinklingStars;
+        private boolean renderNebulas;
+        private boolean nebulasInOtherDimensions;
+
+        private NebulaConfigData nebulaConfig;
+
+        private ConfigData(boolean tweakedLigthmap, boolean twinklingStars, boolean nebulas, String nebulaType, float nebulaStrength, float nebulaNoiseAmount, float nebulaNoiseScale, int baseColourAmount, boolean renderDuringDay) {
+            this.lightmapTweaked = tweakedLigthmap;
+            this.twinklingStars = twinklingStars;
+            this.renderNebulas = nebulas;
+
+            this.nebulaConfig = new NebulaConfigData(nebulaType, nebulaStrength, nebulaNoiseAmount, nebulaNoiseScale, baseColourAmount, renderDuringDay);
         }
 
-        private String nebulaType;
-        private float nebulaStrength;
-        private float nebulaNoiseAmount;
-        private float nebulaNoiseScale;
-        private int baseColourAmount;
-        private boolean renderDuringDay;
+        @Data
+        @AllArgsConstructor
+        @Builder(toBuilder=true)
+        public static final class NebulaConfigData {
+            private String nebulaType;
+            private float nebulaStrength;
+            private float nebulaNoiseAmount;
+            private float nebulaNoiseScale;
+            private int baseColourAmount;
+            private boolean renderDuringDay;
+        }
     }
 }
